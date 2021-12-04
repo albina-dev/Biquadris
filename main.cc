182c27ba9e7a7ca35e07b30ec9b3e0346f1cab77
Albina <54125573+albina-dev@users.noreply.github.com>
2021-12-04 13:03:46 -0500

    Create main.cc

---
+++ main.cc
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int main() {                            
+    cout << "Due to the university's academic integrity policies, the source code can be seen upon request." << endl; 
+}
